a785557f6d7935311af340dbafb5a1452eb36774
Habeeb <habebenasr4@gmail.com>
2025-04-17 10:11:57 +0200

    remove api key

--- firebase/client.ts
+++ firebase/client.ts
@@ -9,7 +9,7 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDeA9k48GhuM9F4lT8gBb-mCAFmhDA_qmA",
+  apiKey: process.env.FIREBASE_API_KEY,
   authDomain: "prephabeeb.firebaseapp.com",
   projectId: "prephabeeb",
   storageBucket: "prephabeeb.firebasestorage.app",
